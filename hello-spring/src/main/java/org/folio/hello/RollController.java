b4ea6a2ca036f8fb6801b749b29d53830ab4c929
Paxxxx <ntnguyen11@crimson.ua.edu>
2023-01-09 16:24:04 -0600

    Create /roll endpoint which simulates rolling a dice

---
+++ hello-spring/src/main/java/org/folio/hello/RollController.java
@@ -0,0 +1,37 @@
+package org.folio.hello;
+
+import java.util.Random;
+
+import org.folio.hello.api.RollApi;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public final class RollController implements RollApi{
+
+    private Random random = new Random();
+
+
+    /** {@inheritDoc}} */
+    @Override
+    public ResponseEntity<Integer> rollGet() {
+        Integer response = random.nextInt(6) + 1;
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    /** {@inheritDoc}} */
+    @Override
+    public ResponseEntity<List<Integer>> rollPost(Integer n) {
+        List<Integer> response = new ArrayList<Integer>(n);
+        for (int i = 0; i < n; ++i) {
+            response.add(random.nextInt(6) + 1);
+        }
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
